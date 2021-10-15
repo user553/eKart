14cbc82165e8cefd26206951e765605f0796e82c
Neetash Pataria <npataria30@hotmail.com>
2021-10-15 01:22:54 +0100

    Checkout process completed

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -45,6 +45,20 @@ def registerUser(request):
         message = {'detail': 'User with this email already exist'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['username']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+    user.save()
+    return Response(serializer.data)
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
